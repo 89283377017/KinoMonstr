41c265cd41136f15e1ef46f210db8f7e1af78435
PrivateName <9034459505@mail.ru>
2020-01-10 04:05:26 +0300

    newVersion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
+
 // function onclickDivergent2(){
-//     alert('Функция сработала');
-//     $('#TTR12').css({'color':'red'});
+//     $('#htpsjt').attr('src', 'https://www.youtube.com/embed/LNaph4ub6pI');
 // }
 
+function onclickDivergent2 (event) {
+    $("#htpsjt").attr("src", "https://www.youtube.com/embed/LNaph4ub6pI");
+    $("#divergentH1").text("Дивергент, глава 2");
+}
 
+
+
+$(document).ready(function () {
+    $(".classA").bind("click", onclickDivergent2);
+});
