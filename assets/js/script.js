59633d45d405c1be8ceaa6263d0002e76f18e2c0
PrivateName <9034459505@mail.ru>
2020-01-12 01:04:43 +0300

    endOfTheProject

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,47 @@
+$("#Divergent1Id").bind("click", function() {
+    $('#iframeId').attr('src', 'https://www.youtube.com/embed/Kj8vhIjUXmM');
+ });
 
-// function onclickDivergent2(){
-//     $('#htpsjt').attr('src', 'https://www.youtube.com/embed/LNaph4ub6pI');
-// }
+ $("#Divergent2Id").bind("click", function() {
+    $('#iframeId').attr('src', 'https://www.youtube.com/embed/LNaph4ub6pI');
+ });
 
-function onclickDivergent2 (event) {
-    $("#htpsjt").attr("src", "https://www.youtube.com/embed/LNaph4ub6pI");
-    $("#divergentH1").text("Дивергент, глава 2");
-}
+ $("#divergent3Id").bind("click", function() {
+    $('#iframeId').attr('src', 'https://www.youtube.com/embed/-vhO6E5wJP4');
+ });
 
+ $("#pompeiiId").bind("click", function() {
+    $('#iframeId').attr('src', 'https://www.youtube.com/embed/y2J4ETfLT1Q');
+ });
 
+ $("#godsId").bind("click", function() {
+    $('#iframeId').attr('src', 'https://www.youtube.com/embed/ul8MGSF1Jwg');
+ });
 
-$(document).ready(function () {
-    $(".classA").bind("click", onclickDivergent2);
-});
+ $("#giverId").bind("click", function() {
+    $('#iframeId').attr('src', 'https://www.youtube.com/embed/8bF36aMx9hI');
+ });
+
+ $("#upsideDownId").bind("click", function() {
+    $('#iframeId').attr('src', 'https://www.youtube.com/embed/SrdpLNCQgMM');
+ });
+
+ $("#midnightSunId").bind("click", function() {
+    $('#iframeId').attr('src', 'https://www.youtube.com/embed/TrJ_uOSB2to');
+ });
+
+ $("#pirate4Id").bind("click", function() {
+    $('#iframeId').attr('src', 'https://www.youtube.com/embed/1QFKtB9K7n8');
+ });
+
+ $("#pirate3Id").bind("click", function() {
+    $('#iframeId').attr('src', 'https://www.youtube.com/embed/1QFKtB9K7n8');
+ });
+
+ $("#pirate1Id").bind("click", function() {
+    $('#iframeId').attr('src', 'https://www.youtube.com/embed/vX--20kUnQs');
+ });
+
+ $("#pirate2Id").bind("click", function() {
+    $('#iframeId').attr('src', 'https://www.youtube.com/embed/kN2IxbyBE-8');
+ });
